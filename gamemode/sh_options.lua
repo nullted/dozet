0ba72fe16c4be2eda5ab463f7c782f21a75fef1b
Toy323 <kolan200804@gmail.com>
2021-05-11 05:39:53 +0900

    Funny update

--- gamemode/sh_options.lua
+++ gamemode/sh_options.lua
@@ -679,7 +679,7 @@ GM:AddDeployableInfo("prop_drone_hauler",		"Hauler Drone",			"weapon_zs_drone_ha
 GM:AddDeployableInfo("prop_rollermine",			"Rollermine",			"weapon_zs_rollermine")
 GM:AddDeployableInfo("prop_tv",                   	 "TV",                    	"weapon_zs_tv")
 
-GM.MaxSigils = 6
+GM.MaxSigils = 2
 
 GM.DefaultRedeem = CreateConVar("zs_redeem", "2", FCVAR_REPLICATED + FCVAR_ARCHIVE + FCVAR_NOTIFY, "The amount of kills a zombie needs to do in order to redeem. Set to 0 to disable."):GetInt()
 cvars.AddChangeCallback("zs_redeem", function(cvar, oldvalue, newvalue)
