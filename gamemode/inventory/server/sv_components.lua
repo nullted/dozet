3d0f794efcef5c804b637a8a2c43481753539bea
Toy323 <kolan200804@gmail.com>
2021-07-30 08:24:15 +0900

    fix

--- gamemode/inventory/server/sv_components.lua
+++ gamemode/inventory/server/sv_components.lua
@@ -6,18 +6,31 @@ GM.StarterTrinkets = {
 	"trinket_aimaid",
 	"trinket_vitamins",
 	"trinket_welfare",
-	"trinket_chemistry",
-	"trinket_classixsoul"
+	"trinket_chemistry"
+	
 }
 GM.StarterSoul = {
-	"trinket_armband",
-	"trinket_condiments",
-	"trinket_emanual",
-	"trinket_aimaid",
-	"trinket_vitamins",
-	"trinket_welfare",
-	"trinket_chemistry",
-	"trinket_classixsoul"
+	"trinket_bleaksoul",  -- 1
+	"trinket_spiritess",  -- 2
+	"trinket_samsonsoul",  -- 3
+	"trinket_evesoul",  -- 4
+    "trinket_jacobjesausoul",  -- 5
+    "trinket_isaacsoul",  -- 6
+    "trinket_magdalenesoul",  -- 7
+    "trinket_lilithsoul",  -- 8
+    "trinket_whysoul",  -- 9
+    "trinket_blanksoul", -- 10
+    "trinket_classixsoul",  -- 11
+	"trinket_darksoul",  --12
+	"trinket_eriosoul",  --13
+	"trinket_aposoul",  --14
+	"trinket_betsoul",  --15
+	"trinket_lostsoul",  --16
+	"trinket_greedsoul",  --17
+	"trinket_cainsoul",   --18
+	"trinket_lazarussoul",	-- 19
+	"trinket_forsoul",  -- 20
+	"trinket_starsoul"  -- 21
 }
 
 function GM:AddWorldPropConversionRecipe(model, result)
