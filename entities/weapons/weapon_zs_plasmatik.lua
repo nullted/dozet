2c564e4316aeaa3bb6d9a3f41ef193677f9e8a49
Toy323 <83911582+Toy323@users.noreply.github.com>
2021-10-02 19:47:16 +0900

    New model for Plasmatik

--- entities/weapons/weapon_zs_plasmatik.lua
+++ entities/weapons/weapon_zs_plasmatik.lua
@@ -7,23 +7,31 @@ SWEP.Description = "Плазма пушка."
 SWEP.Slot = 2
 SWEP.SlotPos = 0
 
-if CLIENT then
-	SWEP.ViewModelFlip = false
-	SWEP.ViewModelFOV = 60
+SWEP.VElements = {
+	["v_10"] = { type = "Model", model = "models/props_combine/combine_fence01a.mdl", bone = "ValveBiped.square", rel = "", pos = Vector(0.958, 1, 5.431), angle = Angle(-2.362, -6.198, -179.508), size = Vector(0.05, 0.05, 0.05), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+	["v_11"] = { type = "Model", model = "models/props_combine/combine_emitter01.mdl", bone = "ValveBiped.square", rel = "", pos = Vector(-0.09, 0.5, -0.004), angle = Angle(-84.399, 0.4, 11.6), size = Vector(0.123, 0.123, 0.123), color = Color(255, 0, 0, 255), surpresslightning = true, material = "", skin = 0, bodygroup = {} },
+	["v_12"] = { type = "Model", model = "models/props_combine/combine_barricade_bracket01b.mdl", bone = "ValveBiped.square", rel = "", pos = Vector(-0.133, -1, -0.004), angle = Angle(92.83, 174.34, -172.075), size = Vector(0.123, 0.123, 0.123), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+	["v_8"] = { type = "Model", model = "models/props_combine/combine_mine01.mdl", bone = "ValveBiped.square", rel = "", pos = Vector(-0.821, 0, -0.015), angle = Angle(167.431, -106.277, 87.061), size = Vector(0.1, 0.1, 0.1), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+	["v_9"] = { type = "Model", model = "models/props_combine/combinethumper001a.mdl", bone = "ValveBiped.square", rel = "", pos = Vector(0.284, 0, 14.1), angle = Angle(-5.934, -1.203, 176.12601), size = Vector(0.02, 0.02, 0.02), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+}
+SWEP.WElements = {
+	["element_nam2"] = { type = "Model", model = "models/props_combine/combine_emitter01.mdl", bone = "ValveBiped.Bip01_R_Finger0", rel = "", pos = Vector(4.025, -3.019, -3.019), angle = Angle(144.13, 71.259, 116.716), size = Vector(0.1, 0.1, 0.1), color = Color(255, 0, 0, 255), surpresslightning = true, material = "", skin = 0, bodygroup = {} },
+	["element_name"] = { type = "Model", model = "models/props_combine/combine_mine01.mdl", bone = "ValveBiped.Anim_Attachment_RH", rel = "", pos = Vector(0, -2.013, 2.642), angle = Angle(73.795, 129.05701, -129.05701), size = Vector(0.1, 0.1, 0.1), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} },
+	["element_name1"] = { type = "Model", model = "models/props_combine/combinethumper002.mdl", bone = "ValveBiped.Bip01_R_Finger2", rel = "", pos = Vector(5.772, -3.971, -3.175), angle = Angle(-23.673, -47.636, -84.851), size = Vector(0.02, 0.02, 0.02), color = Color(255, 255, 255, 255), surpresslightning = false, material = "", skin = 0, bodygroup = {} }
+}
 
-	SWEP.HUD3DBone = "Vent"
-	SWEP.HUD3DPos = Vector(1, 0, 0)
-	SWEP.HUD3DScale = 0.018
-end
+
 
 SWEP.Base = "weapon_zs_base"
 
-SWEP.HoldType = "ar2"
-
-SWEP.ViewModel = "models/weapons/c_irifle.mdl"
-SWEP.WorldModel = "models/weapons/w_IRifle.mdl"
+SWEP.HoldType = "pistol"
+SWEP.ViewModelFOV = 70
+SWEP.ViewModelFlip = false
+SWEP.ViewModel = "models/weapons/c_pistol.mdl"
+SWEP.WorldModel = "models/weapons/w_pistol.mdl"
+SWEP.ShowViewModel = true
+SWEP.ShowWorldModel = true
 SWEP.UseHands = true
-
 SWEP.CSMuzzleFlashes = false
 
 SWEP.ReloadSound = Sound("Weapon_SMG1.Reload")
