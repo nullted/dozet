d7c60be00f55d147e4132ea113cb5f2ab52ca80c
Toy323 <83911582+Toy323@users.noreply.github.com>
2021-12-21 16:54:55 +0900

    Pre-release

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -5,7 +5,7 @@ GM.Website	=	"https://github.com/erioxis/dozet"
 
 -- No, adding a gun doesn't make your name worth being here.
 GM.Credits = {
-	{"Version", "Release Beta", "0.96"},
+	{"Version", "Pre-Release", "0.974"},
 	{"erioxis", "Phantom coder", "dead"},
 	{"Server in", "december!", "(or april)"},
 }
