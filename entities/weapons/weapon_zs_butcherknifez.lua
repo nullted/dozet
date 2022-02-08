aeb1e4298630f5f527c6a65bc56f4f572f4c86b5
Toy323 <83911582+Toy323@users.noreply.github.com>
2022-02-08 19:13:31 +0900

    Butcher buff

--- entities/weapons/weapon_zs_butcherknifez.lua
+++ entities/weapons/weapon_zs_butcherknifez.lua
@@ -22,3 +22,10 @@ function SWEP:SetNextAttack()
 	local armdelay = owner:GetMeleeSpeedMul()
 	self:SetNextPrimaryFire(CurTime() + self.Primary.Delay * armdelay)
 end
+
+function SWEP:OnMeleeHit(hitent, hitflesh, tr)
+	if hitent:IsValid() and hitent:IsPlayer() and not self.m_BackStabbing and math.abs(hitent:GetForward():Angle().yaw - self:GetOwner():GetForward():Angle().yaw) <= 90 then
+		self.m_BackStabbing = true
+		self.MeleeDamage = self.MeleeDamage * 2
+	end
+end
