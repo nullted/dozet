b78fbe87facfe1b01f318dc45511f5978248ef94
Toy323 <83911582+Toy323@users.noreply.github.com>
2022-02-17 23:26:58 +0900

    Server is coming soon...(if version = 1)

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -5,7 +5,7 @@ GM.Website	=	"https://github.com/erioxis/dozet"
 
 -- No, adding a gun doesn't make your name worth being here.
 GM.Credits = {
-	{"Version", "Pre-Release", "0.98"},
+	{"Version", "Pre-Release", "0.99"},
 	{"erioxis", "Phantom coder", "dead"},
 	{"Server in", "december!", "(or april)"},
 }
