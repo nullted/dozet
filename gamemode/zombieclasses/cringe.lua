b9a9ff531c9f4d49889a19e3492813ba7aff3ef5
Toy323 <83911582+Toy323@users.noreply.github.com>
2022-04-04 18:00:18 +0900

    Cringe has appeared!

--- gamemode/zombieclasses/cringe.lua
+++ gamemode/zombieclasses/cringe.lua
@@ -1,4 +1,4 @@
-CLASS.Name = "The Cringe+"
+CLASS.Name = "The Cringe++"
 CLASS.TranslationName = "class_cringe"
 CLASS.Description = "description_the_tickle_monster"
 CLASS.Help = "controls_the_tickle_monster"
@@ -7,8 +7,8 @@ CLASS.Help = "controls_the_tickle_monster"
 CLASS.KnockbackScale = 0
 CLASS.Wave = 1
 
-CLASS.Health = 30000
-CLASS.Speed = 120
+CLASS.Health = 120000
+CLASS.Speed = 250
 CLASS.Hidden = false
 
 CLASS.FearPerInstance = 1
