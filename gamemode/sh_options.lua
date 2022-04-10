8a8e0e5d9a88d91c7ca212b545aa203ff9d1f23d
Toy323 <83911582+Toy323@users.noreply.github.com>
2022-04-11 06:32:08 +0900

    Some

--- gamemode/sh_options.lua
+++ gamemode/sh_options.lua
@@ -136,6 +136,7 @@ GM.AmmoCache["turret_buckshot"]				= 1
 GM.AmmoCache["turret_assault"]				= 1
 GM.AmmoCache["turret_super"]				= 1
 GM.AmmoCache["scrap"]						= 4
+GM.AmmoCache["repairfield1"]						= 1
 
 GM.AmmoResupply = table.ToAssoc({"ar2", "pistol", "smg1", "357", "xbowbolt", "buckshot", "battery", "pulse", "impactmine", "chemical", "gaussenergy", "scrap", "grenade"})
 
